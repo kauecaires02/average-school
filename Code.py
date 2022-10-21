869ecb15ebf9aaae5174faf291ffb229538ef833
_kaueuak <84874622+kauecaires02@users.noreply.github.com>
2022-10-21 03:19:32 -0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,13 +1,19 @@
-# media-alunos
-system de média de alunos 
+# system de média de alunos 
 
-#  nota dos alunos
+print("Matricula:","01234...")
 
-print("# Matricula..:","01234")
 print("Nome do Aluno:","...")
+
+
 print("---------------------")
-print("Nota 1:", 75)
-print("Nota 2:", 85)
-print("Nota 3:", 80)
+
+#nota 1º sem 
+a=7
+
+#nota 2ºsem
+b=5
+
+       
 print("---------------------")
-print("Media Final..:",80)
+       
+a+b/2
