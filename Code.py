cd7717901544667eb31ff3d8f333fa7769cb927a
_kaueuak <84874622+kauecaires02@users.noreply.github.com>
2022-10-21 02:18:38 -0300

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,13 @@
+# media-alunos
+system de média de alunos 
+
+#  nota dos alunos
+
+print("# Matricula..:","01234")
+print("Nome do Aluno:","...")
+print("---------------------")
+print("Nota 1:", 75)
+print("Nota 2:", 85)
+print("Nota 3:", 80)
+print("---------------------")
+print("Media Final..:",80)
